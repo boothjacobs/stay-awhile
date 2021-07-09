d3ff532faaf9754678ff4731394ee1021fb1ce9b
Sarah <jacobs.b.sarah@gmail.com>
2021-07-08 21:45:21 -0700

    extra staff accounts seeded

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -43,11 +43,120 @@ def seed_users():
                 staff=False,
                 ranch_id=None)
 
+    demo5 = User(full_name='Annie Oakley',
+                email='annie@aa.io',
+                password='password',
+                age='31',
+                phone_number='18005555555',
+                dietary_restrictions='None',
+                emergency_contact=None,
+                staff=True,
+                ranch_id='1')
+
+    demo6 = User(full_name='Calamity Jane',
+                email='jane@aa.io',
+                password='password',
+                age='22',
+                phone_number='18005555555',
+                dietary_restrictions='None',
+                emergency_contact=None,
+                staff=True,
+                ranch_id='2')
+
+    demo7 = User(full_name='Phoebe Ann Mosey',
+                email='pheebs@aa.io',
+                password='password',
+                age='28',
+                phone_number='18005555555',
+                dietary_restrictions='None',
+                emergency_contact=None,
+                staff=True,
+                ranch_id='3')
+
+    demo8 = User(full_name='Lillian Smith',
+                email='lilz@aa.io',
+                password='password',
+                age='26',
+                phone_number='18005555555',
+                dietary_restrictions='None',
+                emergency_contact=None,
+                staff=True,
+                ranch_id='5')
+
+    demo9 = User(full_name='Emmylou Harris',
+                email='emmy@aa.io',
+                password='password',
+                age='39',
+                phone_number='18005555555',
+                dietary_restrictions='None',
+                emergency_contact=None,
+                staff=True,
+                ranch_id='6')
+
+    demo10 = User(full_name='Lucinda Williams',
+                email='lucy@aa.io',
+                password='password',
+                age='50',
+                phone_number='18005555555',
+                dietary_restrictions='None',
+                emergency_contact=None,
+                staff=True,
+                ranch_id='7')
+
+    demo11 = User(full_name='Hank Williams',
+                email='hank@aa.io',
+                password='password',
+                age='42',
+                phone_number='18005555555',
+                dietary_restrictions='None',
+                emergency_contact=None,
+                staff=True,
+                ranch_id='8')
+
+    demo12 = User(full_name='Ned Buntline',
+                email='ned@aa.io',
+                password='password',
+                age='67',
+                phone_number='18005555555',
+                dietary_restrictions='None',
+                emergency_contact=None,
+                staff=True,
+                ranch_id='9')
+
+    demo13 = User(full_name='John Wayne',
+                email='john@aa.io',
+                password='password',
+                age='68',
+                phone_number='18005555555',
+                dietary_restrictions='Vegan',
+                emergency_contact=None,
+                staff=False,
+                ranch_id=None)
+
+    demo14 = User(full_name='Natalie Wood',
+                email='nat@aa.io',
+                password='password',
+                age='36',
+                phone_number='18005555555',
+                dietary_restrictions='None',
+                emergency_contact=None,
+                staff=False,
+                ranch_id=None)
 
     db.session.add(demo)
     db.session.add(demo2)
     db.session.add(demo3)
     db.session.add(demo4)
+    db.session.add(demo5)
+    db.session.add(demo6)
+    db.session.add(demo7)
+    db.session.add(demo8)
+    db.session.add(demo9)
+    db.session.add(demo10)
+    db.session.add(demo11)
+    db.session.add(demo12)
+    db.session.add(demo13)
+    db.session.add(demo14)
     db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE the users table.
