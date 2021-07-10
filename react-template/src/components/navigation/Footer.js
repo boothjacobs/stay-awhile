a990a7db549cf9eeda000265e21777b58622deee
Sarah <jacobs.b.sarah@gmail.com>
2021-07-09 21:20:55 -0700

    footer

--- react-template/src/components/navigation/Footer.js
+++ react-template/src/components/navigation/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
 
     return (
         <footer>
-            <p>This site built by Sarah Booth Jacobs</p>
+            <p>Handmade by Sarah Booth Jacobs</p>
             <div id="about-links">
                 <a href="https://angel.co/u/sarah-booth-jacobs"><i className="fab fa-angellist"></i></a>
                 <a href="https://www.linkedin.com/in/sarah-jacobs-53433923/"><i className="fab fa-linkedin-in"></i></a>
